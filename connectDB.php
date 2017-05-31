7f7cbb13b7b91bd3b3aca4cecdbdcb4d286fa4af
Kittapard Puaime <PUAIME_K@silpakorn.edu>
2017-06-01 00:28:52 +0700

    Add files via upload

--- connectDB.php
+++ connectDB.php
@@ -1,8 +1,8 @@
 <?php 
  	$DB_host = "localhost";
- 	$DB_user = "root";
- 	$DB_pass = "";
- 	$DB_name = "craftprize";
+ 	$DB_user = "u13570110";
+ 	$DB_pass = "T8Bg3tkTVn";
+ 	$DB_name = "db13570110";
 
  	$mysqli = new mysqli($DB_host,$DB_user,$DB_pass,$DB_name) OR die("Connection Error!");
 
